47b08d7507cd635e25ff6c86ff579f370acf5286
Yoonjin <peacemiller@konkuk.ac.kr>
2024-07-30 16:14:04 +0900

    create : WriteScreen

--- app/src/main/java/yj/mentalai/view/write/WriteScreen.kt
+++ app/src/main/java/yj/mentalai/view/write/WriteScreen.kt
@@ -1,26 +1,41 @@
 package yj.mentalai.view.write
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import yj.mentalai.R
 import yj.mentalai.ui.theme.MentalAITheme
+import yj.mentalai.ui.theme.Purple40
 import yj.mentalai.ui.theme.PurpleGrey80
-import yj.mentalai.view.home.DiaryList
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -53,10 +68,47 @@ fun WriteScreen() {
                 .fillMaxSize()
                 .padding(it)
                 .padding(16.dp)
+                .imePadding()
+                .verticalScroll(rememberScrollState()),
+            verticalArrangement = Arrangement.spacedBy(10.dp)
         ) {
+            var statement by remember { mutableStateOf(String()) }
+            val keyboardController = LocalSoftwareKeyboardController.current
+
+            Text(text = "7월 30일")
+            Text(
+                text = ("오늘 어떤 감정을 느꼈는지, 어떤 하루를 보냈는지 기록해보세요"),
+                fontSize = 13.sp
+            )
+            TextField(
+                value = statement,
+                onValueChange = { statementField ->
+                    statement = statementField
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+                    ,
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = PurpleGrey80,
+                    unfocusedContainerColor = PurpleGrey80,
+                    focusedIndicatorColor = Purple40,
+                    unfocusedIndicatorColor = Purple40
+                ),
+                keyboardActions = KeyboardActions(
+                    onDone = { keyboardController?.hide() }
+                )
+            )
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = Modifier.fillMaxWidth(),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Purple40
+                )
+            ) {
+                Text(text = "멘탈 분석하기")
+            }
         }
-
-
     }
 }
 
